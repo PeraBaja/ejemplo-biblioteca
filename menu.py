fa39d3e37ebb012271bde0be8640d8fc7c9d3fe6
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-13 15:14:24 -0300

    Corregido seleccionar_menu no muestra el mensaje de error si se ingresa un numero entero que no esté en el rango dado

--- menu.py
+++ menu.py
@@ -2,7 +2,9 @@ def seleccionar_operacion(cantOperaciones: int):
     operacion = 0
     while not (cantOperaciones > operacion > 0):
         try:
-            operacion = int(input('Seleccione una de las operaciones listadas'))
+            operacion = int(input('Seleccione una de las operaciones listadas: '))
+            if not (cantOperaciones > operacion > 0):
+                raise ValueError
         except ValueError:
             print("Error. Seleccione un número de operación de los listados")  
     return operacion
