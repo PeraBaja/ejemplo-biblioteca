92ea6eaa4da6ee2d6c0baa4471cca02a75a2e273
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-13 15:10:17 -0300

    Corregido el error de la funcion seleccionar_operacion()

--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
 def seleccionar_operacion(cantOperaciones: int):
-    operacion = int(input('Seleccione una de las operaciones listadas'))
+    operacion = 0
     while not (cantOperaciones > operacion > 0):
-        print("Error. Seleccione un número de operación de los listados")
-        operacion = int(input('Seleccione una de las operaciones listadas'))
+        try:
+            operacion = int(input('Seleccione una de las operaciones listadas'))
+        except ValueError:
+            print("Error. Seleccione un número de operación de los listados")  
     return operacion
