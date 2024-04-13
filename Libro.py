7d76fa49b905bf99596b6e360263d92de8d6e89e
Benjamin Peralta <benjaminp158@gmail.com>
2024-04-13 18:21:03 -0300

    Refactorizando libro para usar properties

--- Libro.py
+++ Libro.py
@@ -1,50 +1,60 @@
 class Libro:
-  def __init__(self, nombre: str, estado: bool, genero: str, autor: str, codigo: str|int, adulto: bool) :
-    self.__nombre = nombre
-    self.__estado = estado
-    self.__genero = genero
-    self.__autor = autor
-    self.__codigo = codigo
-    self.__adulto = adulto
-
-  def get_nombre(self) :
+  def __init__(self, nombre, estado, genero, autor, codigo, esParaAdultos):
+    self.__nombre: str = nombre
+    self.__estado: bool = estado
+    self.__genero: str = genero
+    self.__autor: str = autor
+    self.__codigo: str = codigo
+    self.__esParaAdultos: bool = esParaAdultos
+  @property
+  def nombre(self) :
     return self.__nombre
-  def get_estado(self) :
+  @property
+  def estado(self) :
     return self.__estado
-  def get_genero(self) :
+  @property
+  def genero(self) :
     return self.__genero
-  def get_autor(self) :
+  @property
+  def autor(self) :
     return self.__autor
-  def get_codigo(self) :
+  @property
+  def codigo(self) :
     return self.__codigo
-  def get_adulto(self) :
-    return self.__adulto
-
-  def set_adulto(self, adulto) :
-    if type(adulto) != bool :
+  @property
+  def esParaAdultos(self):
+    return self.__esParaAdultos
+  @esParaAdultos.setter
+  def set_esParaAdultos(self, esParaAdultos) :
+    if type(esParaAdultos) != bool :
       print('Ingrese un valor booleano')
       exit()
-    self.__adulto = adulto
+    self.__esParaAdultos = esParaAdultos
+  @nombre.setter
   def set_nombre(self, nombre) :
     try: 
       self.__nombre = str(nombre)
     except :
       print('El tipo de valor ingresado debe ser una cadena')
+  @estado.setter
   def set_estado(self, estado) :
     if type(estado) != bool :
       print('Ingrese un valor booleano')
       exit()
     self.__estado = estado
+  @genero.setter
   def set_genero(self, genero) :
     try: 
       self.__genero = str(genero)
     except :
       print('El tipo de valor ingresado debe ser una cadena')
+  @autor.setter
   def set_autor(self, autor) :
     try: 
       self.__autor = str(autor)
     except :
       print('El tipo de valor ingresado debe ser una cadena')
+  @codigo.setter
   def set_codigo(self, codigo) :
     try: 
       self.__codigo = str(codigo)
@@ -53,4 +63,3 @@ class Libro:
 
   def __str__(self) :
     return f'nombre: {self.__nombre}'
-
